67151e6e07007afa8a3d48a3268828658031d813
Adam Slack <adamslack@outlook.com>
2021-12-06 08:38:09 +0000

    2021-6

---
+++ 2021/daySix.js
@@ -0,0 +1,34 @@
+const { readTestInput } = require('./readTestInput')
+
+const initialFishState = readTestInput('six', false)
+  .join('\n')
+  .split(',')
+
+let fishDayCounts = Array
+  .from({length : 9}, () => 0)
+  .reduce((acc, curr, idx) => ({...acc, [`${idx}`]: curr}), {});
+
+initialFishState.forEach((days) => fishDayCounts[days] += 1)
+
+const MAX_DAYS = 256
+let currDay = 0
+
+while(currDay < MAX_DAYS) {
+  const nextDayCounts = {
+    '0': fishDayCounts['1'],
+    '1': fishDayCounts['2'],
+    '2': fishDayCounts['3'],
+    '3': fishDayCounts['4'],
+    '4': fishDayCounts['5'],
+    '5': fishDayCounts['6'],
+    '6': fishDayCounts['0'] + fishDayCounts['7'],
+    '7': fishDayCounts['8'],
+    '8': fishDayCounts['0']
+  }
+  fishDayCounts = nextDayCounts;
+  currDay += 1
+}
+
+console.log(fishDayCounts)
+const count = Object.values(fishDayCounts).reduce((sum, val) => sum + parseInt(val),0)
+console.log(count)
