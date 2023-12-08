5cc1604dae42d909157e47020b574eab9b431b0f
adam slack <adam.slack@nimbleapproach.com>
2023-12-08 11:58:34 +0000

    day 3 part 2

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -1,68 +1,68 @@
 def find_part_numbers(processed_lines):
   part_numbers = []
-  last_part_y = None
   for y in range(len(processed_lines)):
-    last_checked = None
     for x in range(len(processed_lines[y])):
-      is_part_number = False
+      gear_ratios = []
 
-      if not processed_lines[y][x].isdigit():
-        last_part_y = None
+      if processed_lines[y][x] != '*':
         continue
+      print('----------')
+      print(processed_lines[y-1])
+      print(processed_lines[y])
+      print(processed_lines[y+1])
+      print('----------')
 
       # Up
       if y != 0:
         check_char = processed_lines[y-1][x]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y-1][x-1] == '.' or processed_lines[y-1][x+1] == '.'):
+          gear_ratios.append(int(check_char))
 
       # Down
       if y < len(processed_lines)-1:
         check_char = processed_lines[y+1][x]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y+1][x-1] == '.' or processed_lines[y+1][x+1] == '.'):
+          gear_ratios.append(int(check_char))
       
       # Left
       if x != 0:
         check_char = processed_lines[y][x-1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit():
+          gear_ratios.append(int(check_char))
 
       # Right
       if x < len(processed_lines[y])-1:
         check_char = processed_lines[y][x+1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit():
+          gear_ratios.append(int(check_char))
       
       # Up Left
       if y != 0 and x != 0:
         check_char = processed_lines[y-1][x-1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y-1][x] == '.'):
+          gear_ratios.append(int(check_char))
 
       # Up Right
       if y != 0 and x < len(processed_lines[y])-1:
         check_char = processed_lines[y-1][x+1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y-1][x] == '.'):
+          gear_ratios.append(int(check_char))
 
       # Down Left
       if y < len(processed_lines)-1 and x != 0:
         check_char = processed_lines[y+1][x-1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y+1][x] == '.'):
+          gear_ratios.append(int(check_char))
 
       # Down Right
       if y < len(processed_lines)-1 and x < len(processed_lines[y])-1:
         check_char = processed_lines[y+1][x+1]
-        if not check_char.isdigit() and check_char != '.':
-          is_part_number = True
+        if check_char.isdigit() and (int(check_char) not in gear_ratios or processed_lines[y+1][x] == '.'):
+          gear_ratios.append(int(check_char))
 
-      if is_part_number and (last_part_y != y or processed_lines[y][x] != last_checked or not (len(part_numbers) > 0 and part_numbers[len(part_numbers) - 1] == processed_lines[y][x])):
-        last_part_y = y
-        part_numbers.append(processed_lines[y][x])
-
-      last_checked = processed_lines[y][x]
+      if len(gear_ratios) == 2:
+        print(gear_ratios)
+        part_numbers.append(gear_ratios[0] * gear_ratios[1])
 
   return part_numbers
 
@@ -89,7 +89,7 @@ def read_file(filename):
 
 def main():
   tests = ['test.txt', 'test2.txt', 'test3.txt', 'input.txt']
-  answers = [4361, 925, 413, 1]
+  answers = [4361, 6756, 6756, 1]
   for test_idx in range(len(tests)):
     print('\n\nTest', test_idx+1)
     lines = read_file(tests[test_idx])
